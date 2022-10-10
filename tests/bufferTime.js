8a883d75d30644cf50a88bedaeab094b697a000b
byungi <zeronin2@gmail.com>
2022-10-11 00:41:31 +0900

    wip

---
+++ tests/bufferTime.js
@@ -0,0 +1,55 @@
+import test from 'ava'
+import bufferTime from '../lib/bufferTime.js'
+import interval from '../lib/interval.js'
+import take from '../lib/take.js'
+import FakerTimers from '@sinonjs/fake-timers'
+
+const clock = FakerTimers.install()
+test.after(() => clock.uninstall())
+
+test.serial('bufferTime', async t => {
+    const values = []
+    interval(2)
+        .pipe(take(10), bufferTime(5))
+        .subscribe({
+            next(value) {
+                values.push(value)
+            },
+            error: t.log,
+        })
+    await clock.runAllAsync()
+    t.deepEqual(values, [
+        [0, 1],
+        [2, 3, 4],
+        [5, 6],
+        [7, 8, 9],
+    ])
+})
+
+test.serial('creationInterval', async t => {
+    const values = []
+    interval(2)
+        .pipe(take(3), bufferTime(3, 2))
+        .subscribe({
+            next(value) {
+                values.push(value)
+            },
+            error: t.log,
+        })
+    await clock.runAllAsync()
+    t.deepEqual(values, [[0], [0, 1], [1, 2], [2]])
+})
+
+test.serial('limit buffer size', async t => {
+    const values = []
+    interval(20)
+        .pipe(take(5), bufferTime(50, null, 2))
+        .subscribe({
+            next(value) {
+                values.push(value)
+            },
+            error: t.log,
+        })
+    await clock.runAllAsync()
+    t.deepEqual(values, [[0, 1], [2, 3], [4]])
+})
